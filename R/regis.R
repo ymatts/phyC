473a28d0ed69b63bd493ab5db7b35fd986025c0b
Yusuke Matsui <matty@Yusuke-no-MacBook-Air.local>
2016-06-04 13:45:36 -0500

    create metatree is updated for fast computation

--- R/regis.R
+++ R/regis.R
@@ -9,7 +9,8 @@ regis <- function(edgelist,edgeLenList){
   maxdep <- depth.max(resolv)
   cat("creating maximum tree\n")
   cat(paste0("depth=",maxdep," and #leaves=",2^(maxdep),"\n"))
-  meta <- create.metaTree(maxdep+1)
+  #meta <- create.metaTree(maxdep+1)
+  meta <- stree(ntips <- 2^(depth-1+1),"balanced")
   cat("starting encoding trees to maximum tree\n")
   regis <- vector("list",length(resolv))
   for(i in seq_along(resolv)){
